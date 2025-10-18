b7f57dc7fa4e17bf82db0842369dd5418ead9c87
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-10-19 00:33:04 +0900

    fix: remove unused Snowflake variables from variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,6 @@
 variable "SNOWFLAKE_ORGANIZATION" {
   description = "Snowflake organization name"
   type        = string
-
 }
 
 variable "SNOWFLAKE_ACCOUNT" {
@@ -14,12 +13,6 @@ variable "SNOWFLAKE_USER" {
   type        = string
 }
 
-variable "SNOWFLAKE_PASSWORD" {
-  description = "Snowflake password"
-  type        = string
-  sensitive   = true
-}
-
 variable "SNOWFLAKE_ROLE" {
   description = "Snowflake role"
   type        = string
@@ -28,39 +21,17 @@ variable "SNOWFLAKE_ROLE" {
 variable "SNOWFLAKE_PRIVATE_KEY_PATH" {
   description = "Path to the Snowflake private key file"
   type        = string
+  default     = ""
 }
 
 variable "SNOWFLAKE_PRIVATE_KEY_PASSPHRASE" {
   description = "Passphrase for the Snowflake private key"
   type        = string
   sensitive   = true
+  default     = ""
 }
 
 variable "SNOWFLAKE_WAREHOUSE" {
   description = "Snowflake warehouse"
   type        = string
-}
-
-variable "SNOWFLAKE_PREVIEW_FEATURES" {
-  description = "List of preview features to enable for the Snowflake provider."
-  type        = list(string)
-  default     = []
-}
-
-variable "DBT_USER_RSA_PUBLIC_KEY" {
-  description = "Primary RSA public key for the dbt service user (PEM body without headers)."
-  type        = string
-  default     = null
-}
-
-variable "DBT_USER_RSA_PUBLIC_KEY_2" {
-  description = "Secondary RSA public key for the dbt service user (optional, for rotation)."
-  type        = string
-  default     = null
-}
-
-variable "DBT_PARENT_ROLES" {
-  description = "Roles that should inherit the dbt execution role."
-  type        = list(string)
-  default     = []
 }
