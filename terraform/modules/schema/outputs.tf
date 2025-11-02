3199948c31771c31241d4b6aa329d72b050e6efb
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-11-02 22:55:57 +0900

    feat: add output for schema fully qualified name in Terraform module

--- terraform/modules/schema/outputs.tf
+++ terraform/modules/schema/outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   description = "Schema name."
   value       = snowflake_schema.this.name
 }
+
+output "schema_fully_qualified_name" {
+  description = "Schema fully qualified name."
+  value       = snowflake_schema.this.fully_qualified_name
+}
