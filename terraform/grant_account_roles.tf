6077c37f05865ac80d9016c9b8698360f2173eec
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-11-22 22:59:21 +0900

    grant role to dbt_user

--- terraform/grant_account_roles.tf
+++ terraform/grant_account_roles.tf
@@ -13,3 +13,11 @@ module "grant_developer_role_to_kohta" {
   role_name = local.account_role["developer"].name
   user_name = module.kohta_user.name
 }
+
+module "grant_administrator_role_to_dbt_user" {
+  source     = "./modules/grant_account_role/account_role_to_user"
+  depends_on = [module.account_roles, module.dbt_user]
+
+  role_name = local.account_role["administrator"].name
+  user_name = module.dbt_user.name
+}
