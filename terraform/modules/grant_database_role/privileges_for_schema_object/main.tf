75258e6cbc3197a928f5f1ee0ffa3a0ae84525de
KOHTA0405 <koh.yoshi.0405@gmail.com>
2025-11-23 01:54:55 +0900

    add future grants

--- terraform/modules/grant_database_role/privileges_for_schema_object/main.tf
+++ terraform/modules/grant_database_role/privileges_for_schema_object/main.tf
@@ -1,9 +1,23 @@
+# Grant privileges on existing objects
 resource "snowflake_grant_privileges_to_database_role" "this" {
   provider           = snowflake.security_admin
   database_role_name = var.database_role_name
   privileges         = var.privilege_list
   on_schema_object {
     all {
+      object_type_plural = var.object_type
+      in_schema          = var.schema_name
+    }
+  }
+}
+
+# Grant privileges on future objects
+resource "snowflake_grant_privileges_to_database_role" "future" {
+  provider           = snowflake.security_admin
+  database_role_name = var.database_role_name
+  privileges         = var.privilege_list
+  on_schema_object {
+    future {
       object_type_plural = var.object_type
       in_schema          = var.schema_name
     }
