d656ebbcf1f7d4eb27a0df44248aa2d9d399ea43
KOHTA0405 <koh.yoshi.0405@gmail.com>
2026-09-26 00:52:21 +0900

    スキーマレベルのexternal volume設定の削除

--- terraform/snowflake/schemas.tf
+++ terraform/snowflake/schemas.tf
@@ -5,7 +5,6 @@ module "schemas" {
   database                     = module.database.name
   name                         = each.value.name
   comment                      = each.value.comment
-  external_volume              = each.key == "gold" && local.iceberg_storage != null ? snowflake_external_volume.iceberg[local.environment].name : null
   storage_serialization_policy = each.key == "gold" && local.iceberg_storage != null ? "COMPATIBLE" : null
 
   providers = {
